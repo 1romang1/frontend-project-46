e60dad68383e613d7610b4b1944fd9251ac8a7de
1romang1 <krglrv@gmail.com>
2024-02-05 17:30:30 +0300

    add arguments in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,11 @@ program
   .version("0.0.1", "-V, --version", "output the version number")
   .description("Compares two configuration files and shows a difference.")
   .helpOption("-h, --help", "output usage information")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
-  .option("-f, --format [type]", "output format");
-
+  .argument("<filepath1>", 'path to file1')
+  .argument("<filepath2>", 'path to file2')
+  .option("-f, --format [type]", "output format")
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+  })
 program.parse();
