de31792ade1e5db11e47959fde08093f3f1c1e2b
1romang1 <krglrv@gmail.com>
2024-02-06 11:13:12 +0300

    fix jsonParsw.js

--- jsonParse.js
+++ jsonParse.js
@@ -1,13 +1,6 @@
 import * as fs from "node:fs";
-import path from "node:path";
 
-const getPath = (fileName) => {
-  return path.resolve(fileName);
-};
-
-const jsonParse = (filename) => {
-  const filePath = getPath(filename);
-//   console.l(path.extname(filePath))
+const jsonParse = (filePath) => {
   const data = fs.readFileSync(filePath, "utf8");
   return JSON.parse(data);
 };
