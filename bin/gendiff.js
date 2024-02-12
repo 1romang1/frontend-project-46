4fdd2cacdfecd198ffefc024b022ede458dea09e
1romang1 <krglrv@gmail.com>
2024-02-12 14:51:40 +0300

    fix genDiff.js to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { genDiff } from '../testInProdaction.js';
+import { gendiff } from '../testInProdaction.js';
 
 const program = new Command();
 
