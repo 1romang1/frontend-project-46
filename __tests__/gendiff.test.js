369841e84794d2b07e9c02a76a6c353ad72edde7
1romang1 <krglrv@gmail.com>
2024-02-14 16:34:31 +0300

    fix __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,6 @@
-// import pkg from '@jest/globals';
-
-import { jsonParse, genDiff } from '../sandbox.js';
-
-// const { test, expect } = pkg;
-// import { genDiff } from "../bin/gendiff.js";
+import { jsonParse, genDiff } from '../index.js';
 
 const result = jsonParse('./__fixtures__/result.json');
-// console.log(JSON.stringify(result))
-
-// const temp = genDiff('file1.json', 'file2.json');
-// console.log(JSON.stringify(temp))
 
 test('gendiff', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(result);
