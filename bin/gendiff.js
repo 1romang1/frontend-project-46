a9e2e5514e52e087273cd221ce15935d8a4cf7c3
1romang1 <krglrv@gmail.com>
2024-02-14 16:33:07 +0300

    fix bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,40 +1,6 @@
 #!/usr/bin/env node
-import * as fs from 'node:fs';
 import { Command } from 'commander';
-
-export const jsonParse = (filePath) => {
-  const data = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(data);
-};
-
-export const genDiff = (filePath1, filePath2) => {
-  const obj1 = jsonParse(filePath1);
-  const obj2 = jsonParse(filePath2);
-  const keys1 = Object.keys(obj1).sort();
-  const keys2 = Object.keys(obj2).sort();
-
-  const result = [];
-
-  keys1.forEach((key) => {
-    if (Object.hasOwn(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        result.push(`  ${key}: ${obj1[key]}`);
-      } else {
-        result.push(`- ${key}: ${obj1[key]}`);
-        result.push(`+ ${key}: ${obj2[key]}`);
-      }
-    } else {
-      result.push(`- ${key}: ${obj1[key]}`);
-    }
-  });
-
-  keys2.forEach((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      result.push(`+ ${key}: ${obj2[key]}`);
-    }
-  });
-  return result.join('\n');
-};
+import { genDiff } from '../index.js';
 
 const program = new Command();
 
@@ -49,5 +15,3 @@ program
     console.log(genDiff(filepath1, filepath2));
   });
 program.parse();
-
-// export { jsonParse, genDiff };
