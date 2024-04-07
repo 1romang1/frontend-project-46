f86eb8591b2248e81004b5017c4d90b2903183d2
Roma <krglrv@gmail.com>
2024-04-07 16:53:44 +0300

    fix gendiff to nested objects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import _ from 'lodash';
-import parse from './parsers.js';
+import _ from "lodash";
+import parse from "./parsers.js";
 
 const genDiff = (filePath1, filePath2) => {
   const obj1 = parse(filePath1);
@@ -10,11 +10,11 @@ const genDiff = (filePath1, filePath2) => {
 
   const result = keys.map((key) => {
     if (!Object.hasOwn(obj1, key)) {
-      return { key, value: obj2[key], status: 'added' };
+      return { key, value: obj2[key], status: "added" };
     }
 
     if (!Object.hasOwn(obj2, key)) {
-      return { key, value: obj1[key], status: 'deleted' };
+      return { key, value: obj1[key], status: "deleted" };
     }
 
     if (obj1[key] !== obj2[key]) {
@@ -22,14 +22,20 @@ const genDiff = (filePath1, filePath2) => {
         key,
         value: obj1[key],
         changedValue: obj2[key],
-        status: 'changed',
+        status: "changed",
       };
     }
 
-    return { key, value: obj1[key], status: 'unchanged' };
+    if (typeof obj1[key] === "object" && typeof obj2[key] === "object") {
+      return { key, value: genDiff(obj1[key], obj2[key]), status: 'whithChildren' }
+    }
+
+    return { key, value: obj1[key], status: "unchanged" };
   });
 
   return result;
 };
 
 export default genDiff;
+
+console.log(JSON.stringify(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json'), null, ' '));
