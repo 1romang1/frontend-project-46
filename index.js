1093f043fe8af64b147e5762fa88188175c2111c
1romang1 <krglrv@gmail.com>
2024-04-11 11:13:38 +0300

    fix code climate issues in genDiff()

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-import _ from "lodash";
-import parse from "./parsers.js";
+import _ from 'lodash';
+import parse from './parsers.js';
 
 const obj1 = parse('./__fixtures__/file1.json');
 const obj2 = parse('./__fixtures__/file2.json');
 
-const genDiff = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const keys = _.union(keys1, keys2).sort();
+const genDiff = (tree1, tree2) => {
+  const keys = _.union(Object.keys(tree1), Object.keys(tree2)).sort();
 
   const result = keys.map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      return { key, value: obj2[key], status: "added" };
+    if (!Object.hasOwn(tree1, key)) {
+      return { key, value: obj2[key], status: 'added' };
     }
 
-    if (!Object.hasOwn(obj2, key)) {
-      return { key, value: obj1[key], status: "deleted" };
+    if (!Object.hasOwn(tree2, key)) {
+      return { key, value: tree1[key], status: 'deleted' };
     }
 
-    if (obj1[key] !== obj2[key]) {
-      if (typeof obj1[key] === "object" && obj1[key] !== null && typeof obj2[key] === "object" && obj2[key] !== null) {
-        return { key, value: genDiff(obj1[key], obj2[key]), status: 'withChildrens' };
+    if (tree1[key] !== tree2[key]) {
+      if (
+        typeof tree1[key] === 'object'
+        && tree1[key] !== null
+        && typeof tree2[key] === 'object'
+        && tree2[key] !== null
+      ) {
+        return {
+          key,
+          value: genDiff(tree1[key], tree2[key]),
+          status: 'withChildrens',
+        };
       }
       return {
         key,
-        value: obj1[key],
-        changedValue: obj2[key],
-        status: "changed",
+        value: tree1[key],
+        changedValue: tree2[key],
+        status: 'changed',
       };
     }
-    return { key, value: obj1[key], status: "unchanged" };
+    return { key, value: tree1[key], status: 'unchanged' };
   });
 
   return result;
