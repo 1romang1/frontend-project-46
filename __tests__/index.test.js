4c2bc81f5680b622aeb64a7dad4c3202c42dd99b
1romang1 <krglrv@gmail.com>
2024-04-15 11:49:20 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
 import * as fs from 'node:fs';
-import genDiff from '../index.js';
+import buildAstTree from '../index.js';
 import parse from '../parsers.js';
-import { stylish } from '../stylish.js';
+import genDiff from '../sandbox.js';
 
 const result = fs.readFileSync('./__fixtures__/result1.txt', 'utf8');
 // const resultJson = parse('./__fixtures__/result.txt');
 // const resultYml = parse('./__fixtures__/result.txt');
 
 test('gendiff', () => {
-  expect(stylish(genDiff(parse('./__fixtures__/file1.json'), parse('./__fixtures__/file2.json')))).toEqual(result);
+  expect(genDiff(buildAstTree(parse('./__fixtures__/file1.json'), parse('./__fixtures__/file2.json')))).toEqual(result);
   // expect(stylish(genDiff(parse('./__fixtures__/file1.yml'), parse('./__fixtures__/file2.yml')))).toEqual(result);
 });
