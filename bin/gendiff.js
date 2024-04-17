0a37ca333d3e9712d88303994608465050970ec0
1romang1 <krglrv@gmail.com>
2024-04-17 15:19:31 +0300

    fix display --help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,8 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .argument('<filepath1>', 'path to file1')
-  .argument('<filepath2>', 'path to file2')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
     console.log(genDiff(filepath1, filepath2, options.format));
   });
