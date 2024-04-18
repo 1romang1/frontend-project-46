b41b32750f3467c2cad874fac73812c1d4868a87
1romang1 <krglrv@gmail.com>
2024-04-18 14:39:34 +0300

    added currentPath

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import path from 'node:path';
+import { cwd } from 'node:process';
 import parse from './parsers.js';
 import buildAstTree from './buildAstTree.js';
 import formater from './formatters/index.js';
 
 const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
-  const tree1 = parse(filePath1);
-  const tree2 = parse(filePath2);
+  const currentDir = cwd();
+  const currentPath1 = path.resolve(currentDir, filePath1);
+  const currentPath2 = path.resolve(currentDir, filePath2);
+  const tree1 = parse(currentPath1);
+  const tree2 = parse(currentPath2);
   const astTree = buildAstTree(tree1, tree2);
   return formater(astTree, formatName);
 };
