4fdaf1e135ff6b6c80322b4bf026a1ca625b3348
1romang1 <krglrv@gmail.com>
2024-04-18 15:04:53 +0300

    fix code climate issues (buildAstTree)

--- buildAstTree.js
+++ buildAstTree.js
@@ -2,23 +2,15 @@ import _ from 'lodash';
 
 const buildAstTree = (tree1, tree2) => {
   const keys = _.union(Object.keys(tree1), Object.keys(tree2)).sort();
-
   const result = keys.map((key) => {
     if (!Object.hasOwn(tree1, key)) {
       return { key, value: tree2[key], status: 'added' };
     }
-
     if (!Object.hasOwn(tree2, key)) {
       return { key, value: tree1[key], status: 'deleted' };
     }
-
     if (tree1[key] !== tree2[key]) {
-      if (
-        typeof tree1[key] === 'object'
-        && tree1[key] !== null
-        && typeof tree2[key] === 'object'
-        && tree2[key] !== null
-      ) {
+      if (typeof tree1[key] === 'object' && tree1[key] !== null && typeof tree2[key] === 'object' && tree2[key] !== null) {
         return {
           key,
           value: buildAstTree(tree1[key], tree2[key]),
@@ -34,7 +26,6 @@ const buildAstTree = (tree1, tree2) => {
     }
     return { key, value: tree1[key], status: 'unchanged' };
   });
-
   return result;
 };
 
