02fbe907e749d04f55cf1e4c38fc27ffe0c0483c
1romang1 <krglrv@gmail.com>
2024-04-22 16:58:32 +0300

    fix tests v2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,6 @@
 import * as fs from 'node:fs';
 import genDiff from '../src/index.js';
 
-// const result = fs.readFileSync('./____fixtures____/result1.txt', 'utf8');
-// const resultPlain = fs.readFileSync('./____fixtures____/plain.txt', 'utf8');
-// const resultJson = JSON.stringify(fs.readFileSync('./____fixtures____/result1.txt', 'utf8'));
-
-// test('gendiff', () => {
-//   expect(genDiff('./____fixtures____/file1.json', './____fixtures____/file2.json')).toEqual(result);
-//   expect(genDiff('./____fixtures____/file1.yml', './____fixtures____/file2.yml')).toEqual(result);
-// });
-
-// test('gendiff plain format', () => {
-//   expect(genDiff('./____fixtures____/file1.json', './____fixtures____/file2.json', 'plain')).toEqual(resultPlain);
-//   expect(genDiff('./____fixtures____/file1.yml', './____fixtures____/file2.yml', 'plain')).toEqual(resultPlain);
-// });
-
-// test('gendiff json format', () => {
-//   expect(genDiff('./____fixtures____/file1.json', './____fixtures____/file2.json', 'json')).toEqual(resultJson);
-//   expect(genDiff('./____fixtures____/file1.yml', './____fixtures____/file2.yml', 'json')).toEqual(resultJson);
-// });
 const testData = [
   ['./__fixtures__/file1.json', './__fixtures__/file2.json', undefined, './__fixtures__/result.txt'],
   ['./__fixtures__/file1.yml', './__fixtures__/file2.yml', undefined, './__fixtures__/result.txt'],
