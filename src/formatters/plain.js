6e74be021d7c79981d0efc4a9376f52e2c5fa5ce
1romang1 <krglrv@gmail.com>
2024-04-22 17:40:04 +0300

    used ternary operator in src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,26 +4,26 @@ const stringify = (node) => {
   if (_.isObject(node)) {
     return '[complex value]';
   }
-  if (typeof node === 'string') {
-    return `'${node}'`;
-  }
-  return node;
+  return typeof node === 'string' ? `'${node}'` : node;
 };
 
 const plain = (arr) => {
   const iter = (currentValue, pathToValue) => {
     const result = currentValue.flatMap((item) => {
-      const {
-        key, value, changedValue, status,
-      } = item;
-      const currentPathToValue = pathToValue === '' ? `${key}` : `${pathToValue}.${key}`;
+      const { key, value, changedValue, status } = item;
+      const currentPathToValue =
+        pathToValue === '' ? `${key}` : `${pathToValue}.${key}`;
       switch (status) {
         case 'added':
-          return `Property '${currentPathToValue}' was added with value: ${stringify(value)}`;
+          return `Property '${currentPathToValue}' was added with value: ${stringify(
+            value
+          )}`;
         case 'deleted':
           return `Property '${currentPathToValue}' was removed`;
         case 'changed':
-          return `Property '${currentPathToValue}' was updated. From ${stringify(value)} to ${stringify(changedValue)}`;
+          return `Property '${currentPathToValue}' was updated. From ${stringify(
+            value
+          )} to ${stringify(changedValue)}`;
         case 'withChildren':
           return iter(value, currentPathToValue);
         case 'unchanged':
