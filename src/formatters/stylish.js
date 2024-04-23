eb9e9934471cce8541289827d9a69d5d378b3fbd
1romang1 <krglrv@gmail.com>
2024-04-23 11:08:45 +0300

    fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,28 +1,27 @@
 import _ from 'lodash';
 
+const SPACE_COUNT = 4;
+const REPLACER = ' ';
+
 const stringify = (node, depth) => {
-  const iter1 = (tree, depth1) => {
+  const iter = (tree, iterDepth) => {
     if (!_.isObject(tree)) {
       return `${tree}`;
     }
-    const SPACE_COUNT1 = 4;
-    const REPLACER1 = ' ';
-    const indentSize = depth1 * SPACE_COUNT1;
-    const currentIndent = REPLACER1.repeat(indentSize);
-    const bracketIndent = REPLACER1.repeat(indentSize - SPACE_COUNT1);
+    const indentSize = iterDepth * SPACE_COUNT;
+    const currentIndent = REPLACER.repeat(indentSize);
+    const bracketIndent = REPLACER.repeat(indentSize - SPACE_COUNT);
     const lines = Object.entries(tree).map(
-      ([key, val]) => `${currentIndent}${key}: ${iter1(val, depth1 + 1)}`,
+      ([key, val]) => `${currentIndent}${key}: ${iter(val, iterDepth + 1)}`,
     );
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
   };
 
-  return iter1(node, depth);
+  return iter(node, depth);
 };
 
 const stylish = (arr) => {
   const iter = (currentValue, depth) => {
-    const SPACE_COUNT = 4;
-    const REPLACER = ' ';
     if (!_.isObject(currentValue)) {
       return `  ${currentValue}`;
     }
@@ -40,9 +39,7 @@ const stylish = (arr) => {
         case 'deleted':
           return `${currentIndent}- ${key}: ${stringify(value, depth + 1)}`;
         case 'changed':
-          return `${currentIndent}- ${key}: ${stringify(
-            stringify(value, depth + 1),
-          )}\n${currentIndent}+ ${key}: ${stringify(changedValue, depth + 1)}`;
+          return `${currentIndent}- ${key}: ${stringify(stringify(value, depth + 1))}\n${currentIndent}+ ${key}: ${stringify(changedValue, depth + 1)}`;
         case 'withChildren':
           return `${currentIndent}  ${key}: ${iter(value, depth + 1)}`;
         case 'unchanged':
