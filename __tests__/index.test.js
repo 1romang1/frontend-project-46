713099520b348228dfe78eb2f1e0f00db4eca860
1romang1 <krglrv@gmail.com>
2024-04-26 16:55:03 +0300

    fix path to fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,31 @@
 import * as fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+
+const FILE1_JSON = getFixturePath('file1.json');
+const FILE2_JSON = getFixturePath('file2.json');
+const FILE1_YML = getFixturePath('file1.yml');
+const FILE2_YML = getFixturePath('file2.yml');
+const RESULT = getFixturePath('result.txt');
+const RESULT_PLAIN = getFixturePath('resultPlain.txt');
+const RESULT_JSON = getFixturePath('resultJson.txt');
+
 const testData = [
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', undefined, './__fixtures__/result.txt'],
-  ['./__fixtures__/file1.yml', './__fixtures__/file2.yml', undefined, './__fixtures__/result.txt'],
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'stylish', './__fixtures__/result.txt'],
-  ['./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'stylish', './__fixtures__/result.txt'],
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain', './__fixtures__/resultPlain.txt'],
-  ['./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'plain', './__fixtures__/resultPlain.txt'],
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'json', './__fixtures__/resultJson.txt'],
-  ['./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'json', './__fixtures__/resultJson.txt'],
+  [FILE1_JSON, FILE2_JSON, undefined, RESULT],
+  [FILE1_YML, FILE2_YML, undefined, RESULT],
+  [FILE1_JSON, FILE2_JSON, 'stylish', RESULT],
+  [FILE1_YML, FILE2_YML, 'stylish', RESULT],
+  [FILE1_JSON, FILE2_JSON, 'plain', RESULT_PLAIN],
+  [FILE1_YML, FILE2_YML, 'plain', RESULT_PLAIN],
+  [FILE1_JSON, FILE2_JSON, 'json', RESULT_JSON],
+  [FILE1_YML, FILE2_YML, 'json', RESULT_JSON],
 ];
 
 test.each(testData)('gendiff', (file1, file2, format, expected) => {
